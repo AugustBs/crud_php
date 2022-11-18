c5e6aa849d225dfbd9124865187164ea9912a69d
August Goethe <iaugusts@outlook.com>
2022-11-17 22:12:28 -0500

    first commit

---
+++ app/controllers/guardar.php
@@ -0,0 +1,9 @@
+<?php
+require_once "../models/persona.model.php";
+$arrayName = array(
+  'nombres' => $_POST['nombres'],
+  'email' => $_POST['email'],
+  'edad' => $_POST['edad']
+);
+
+echo json_encode(Persona::guardarDato($arrayName));
